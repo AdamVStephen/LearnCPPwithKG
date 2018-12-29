755c4a88adcc230ac71b74c7d61eb869e43f34fa
Adam Stephen <adam.v.stephen@gmail.com>
2018-12-29 13:12:07 +0000

    Add missing header.

---
+++ CMakeProject1/CMakeProject1/stringsAndVector.h
@@ -0,0 +1,10 @@
+#pragma once
+// TODO : What does #pragma once do ?  Presumably this implements idempotency?
+
+#include <string>
+
+#include <corecrt_wstring.h>
+
+void savDemo();
+void hw_string();
+void hw_string_mandarin();
